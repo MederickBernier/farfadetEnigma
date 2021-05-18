955707e25e489428e06b86cf33cf05e2935a852e
Médérick Bernier <mederick.bernier@hotmail.ca>
2021-05-18 15:42:21 -0400

    added carousel in index.php, but still figuring out sizing

--- assets/js/material.js
+++ assets/js/material.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        //indicators: true,
+        duration:150,
+        padding:200,
+    }, setTimeout(autoplay,4000));
 });
+
+function autoplay(){
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4000);
+}
--- index.php
+++ index.php
@@ -4,7 +4,16 @@ require_once "views/header.php";
 ?>
 
 <div class="row center-align">
-    <h1>Index</h1>
+    <div class="carousel carousel-slider">
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_09_o.jpg" atl="image1"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_11_o.jpg" atl="image2"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_15_o.jpg" atl="image3"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_16_o.jpg" atl="image4"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_19_o.jpg" atl="image5"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_20_o.jpg" atl="image6"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_23_o.jpg" atl="image7"></a>
+        <a class="carousel-item" href="#"><img class="carouselImg" src="assets/img/diaporama_10-diaporama_24_o.jpg" atl="image8"></a>
+    </div>
 </div>
 
 <?php
